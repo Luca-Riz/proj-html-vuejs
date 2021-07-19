0d0e2ff3526530ac3a2f4440de59559c7f74470b
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-19 12:43:28 +0200

    add section7

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,6 +6,7 @@
     <Section4/>
     <Section5/>
     <Section6/>
+    <Section7/>
     
   </div>
 </template>
@@ -17,6 +18,7 @@ import Section3 from './Section3.vue'
 import Section4 from './Section4.vue'
 import Section5 from './Section5.vue'
 import Section6 from './Section6.vue'
+import Section7 from './Section7.vue'
 
 export default {
   name: 'Main',
@@ -27,6 +29,7 @@ export default {
     Section4,
     Section5,
     Section6,
+    Section7,
   },
   props: {
     msg: String
---
+++ src/components/Section7.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="image debug">
+      <img src="../assets/img/h1-img-04.jpg" alt="">
+      <div class="play">
+        <i class="bi bi-play-fill"></i>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Section7'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/common.scss';
+
+.image {
+  position: relative;
+  max-width: 100%;
+
+  img{
+    width: 100%;
+  }
+
+  .play{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    font-size: 60px;
+  }
+}
+
+</style>
